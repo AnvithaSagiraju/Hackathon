765013869a070625a20c794d4f6360e7080f697f
Advik Sagiraju <advik.sagiraju@gmail.com>
2024-04-21 09:57:18 -0700

    Added more other users

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var userPoints = 0;
 var userStreak = 0;
 var userName = document.getElementById("textInput");
 var habitMatrix = [["Met sleep goal", 0], ["Exercised 30 minutes", 0], ["Studied 1 hour for Math", 0]];
-var otherUsers = [["Bob", 20], ["Joe", 25], ["Billy", 125], ["Sam", 130]];
+var otherUsers = [["Bob", 20], ["Joe", 25], ["Billy", 125], ["Sam", 130], ["Mark", 85], ["Jim", 50]];
 var weeklyHabitMatrix = [["Did the laundry", 0], ["Took out trash", 0], ["Attended a school seminar", 0]];
 
 var displayPoints = document.getElementById("point_display");
